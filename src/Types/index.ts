bda3c714ba03accf85ee21c21a4d9e55119a09a2
Aidan Richardson <aidan.richardson@me.com>
2025-10-29 15:13:25 +0000

    Improving types and stores to reduce code warnings

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -1,6 +1,11 @@
-type Project = {
+type Item = {
 	id: number;
 	image: string[];
 	title: string;
 	description: string;
+};
+
+type List = {
+	title: string;
+	items: string[];
 };
--- src/stores/modal.ts
+++ src/stores/modal.ts
@@ -1,4 +1,6 @@
 import { writable } from 'svelte/store';
 
-export const showModal = writable(false);
-export const selectedProject = writable(null);
+export const showPreviewModal = writable(false);
+export const selectedItem = writable<Item | null>(null);
+export const selectedList = writable<List | null>(null);
+export const showListModal = writable(false);
