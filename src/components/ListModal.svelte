7e8b9f7e9705a2f128d7037acf995bde019d9b94
Aidan Richardson <aidan.richardson@me.com>
2025-11-05 23:08:31 +0000

    Added scrolling to list modal and updated profile, new skills and languages

--- src/components/ListModal.svelte
+++ src/components/ListModal.svelte
@@ -26,22 +26,23 @@
 		<!-- svelte-ignore a11y_click_events_have_key_events -->
 		<!-- svelte-ignore a11y_no_static_element_interactions -->
 		<div
-			class="relative z-10 flex flex-row justify-center overflow-hidden rounded-2xl bg-base-100 text-base-content shadow-lg"
+			class="relative z-10 flex max-h-1/2 w-full max-w-sm flex-col overflow-hidden rounded-2xl bg-base-100 text-base-content shadow-lg"
 			on:click|stopPropagation
 		>
-			<div class="max-w-sm min-w-sm overflow-hidden">
-				<div class="flex items-center justify-between border-b border-base-300 px-6 py-1">
-					<h2 class="flex-1 text-center text-xl font-bold">{$selectedList.title}</h2>
-					<button
-						type="button"
-						on:click={close}
-						class="ml-4 cursor-pointer text-3xl"
-						aria-label="Close modal"
-					>
-						&times;
-					</button>
-				</div>
-				<ul class="list-none space-y-2 px-6 py-2">
+			<div class="flex items-center justify-between border-b border-base-300 px-6 py-2">
+				<h2 class="flex-1 text-center text-xl font-bold">{$selectedList.title}</h2>
+				<button
+					type="button"
+					on:click={close}
+					class="ml-4 cursor-pointer text-3xl"
+					aria-label="Close modal"
+				>
+					&times;
+				</button>
+			</div>
+
+			<div class="flex-1 overflow-y-scroll px-6 py-4">
+				<ul class="list-none space-y-3">
 					{#each $selectedList.list_items as item}
 						<li class="flex items-center justify-between text-lg">
 							<span>{item.name}</span>
